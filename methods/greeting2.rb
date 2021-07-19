c9eb2886e4b453e303592ca52f1b9e6b1d3acc48
Kurt Kerns <kurtkerns20@gmail.com>
2021-07-18 21:05:50 -0500

    Completed exercises for methods in ruby basics on LS.

---
+++ methods/greeting2.rb
@@ -0,0 +1,11 @@
+# This is how I initially thought to complete greeting1.rb
+
+def hello
+  "Hello"
+end
+
+def world
+  "World"
+end
+
+puts hello + " " + world
---
+++ methods/make_model.rb
@@ -0,0 +1,6 @@
+# Further Exploration
+  "#{make} #{model}"
+end
+
+puts car('Toyota', 'Corolla') 
+# These will be the return values without anything to print
